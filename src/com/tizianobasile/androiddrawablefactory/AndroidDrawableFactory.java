749028a6219ac48f670cd3f4280fca1276f2ec8f
Tiziano Basile <tiz.basile@gmail.com>
2014-01-13 21:35:26 +0100

    fixed LaF on Windows, added splashscreen

--- src/com/tizianobasile/androiddrawablefactory/AndroidDrawableFactory.java
+++ src/com/tizianobasile/androiddrawablefactory/AndroidDrawableFactory.java
@@ -12,16 +12,37 @@ public class AndroidDrawableFactory {
 	public static final double[] DENSITY_MULTIPLIERS = {.75, 1, 1.5, 2, 3, 4};
 	public static void main(String... args)
 	{
-		try
-		{
-			UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
-		}
-		catch(UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e){e.printStackTrace();}
+		initLaF();
 		Main mainWindow = new Main();
 		mainWindow.pack();
+		mainWindow.setTitle("Android Drawable Factory");
 		mainWindow.setLocationRelativeTo(null);
 		mainWindow.setResizable(false);
 		mainWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainWindow.setVisible(true);
+	}
+	
+	private static void initLaF()
+	{
+		UIManager.LookAndFeelInfo[] lafs = UIManager.getInstalledLookAndFeels();
+		try
+		{
+			boolean hasGTK = false;
+			for(UIManager.LookAndFeelInfo info : lafs)
+			{
+				if(info.getName().equals("GTK+"))
+				{
+					hasGTK = true;
+					break;
+				}
+			}
+			if(hasGTK)
+			{
+				UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+			}
+			else
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		}
+		catch(UnsupportedLookAndFeelException | ClassNotFoundException | InstantiationException | IllegalAccessException e){e.printStackTrace();}
 	}
 }
